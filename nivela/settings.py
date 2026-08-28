28eb7419f820b6eb98d35ecd09ca267befcba9b7
Beatriz Miguel <beatrizt.miguel2002@gmail.com>
2026-08-27 21:18:15 -0300

    Configurando conexao com PostgreSQL e cria apps usuarios, turmas, gamificacao e chat

--- nivela/settings.py
+++ nivela/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'usuarios',
+    'turmas',
+    'gamificacao',
+    'chat',
 ]
 
 MIDDLEWARE = [
@@ -72,13 +76,15 @@ WSGI_APPLICATION = 'nivela.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+DATABASES = {
+     'default': { 
+         'ENGINE': 'django.db.backends.postgresql', 
+         'NAME': 'nivela_db', 'USER': 'postgres', 
+         'PASSWORD': 'SENHA_REMOVIDA', 'HOST': 'localhost', 
+         'PORT': '5433', 
+    } 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
