909293edb3ce72ea0fa46bbfc3fa9c205b379ed5
Beatriz Miguel <beatrizt.miguel2002@gmail.com>
2026-09-23 22:22:18 -0300

    feat(lgpd): implementacao consulta, exportacao e exclusao dos dados do titular (itens 4.8, 4.9 e 4.10)

--- lgpd/urls.py
+++ lgpd/urls.py
@@ -5,6 +5,9 @@ from . import views
 app_name = 'lgpd'
 
 urlpatterns = [
+    path('meus-dados/', views.meus_dados, name='meus_dados'),
+    path('meus-dados/exportar/', views.exportar_dados, name='exportar_dados'),
+    path('meus-dados/excluir/', views.excluir_conta, name='excluir_conta'),
     path('meus-dados/consentimentos/', views.meus_consentimentos, name='meus_consentimentos'),
     path('meus-dados/consentimentos/<int:pk>/revogar/', views.revogar_consentimento, name='revogar_consentimento'),
 ]
